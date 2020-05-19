65206222927299b8f4d2f88bac15910aefb40ca2
KimYeeun <s2018s01@e-mirim.hs.kr>
2020-05-19 17:09:27 +0900

    숫자 맞추기 게임

--- myrandomrealnumber/Program.cs
+++ myrandomrealnumber/Program.cs
@@ -10,6 +10,29 @@ namespace myrandomrealnumber
     {
         static void Main(string[] args)
         {
+            Random rand = new Random();
+            int number = rand.Next(0,101);
+            int input;
+
+            while (true)
+            {
+                Console.Write("숫자를 입력해주세요>>");
+                input = int.Parse(Console.ReadLine());
+
+                if (number > input)
+                {
+                    Console.WriteLine(input + "보다는 큰 숫자입니다.");
+                }
+                else if(number<input)
+                {
+                    Console.WriteLine(input + "보다는 작은 숫자입니다.");
+                }
+                else
+                {
+                    Console.WriteLine("정답!!");
+                    break;
+                }
+            }
         }
     }
 }
